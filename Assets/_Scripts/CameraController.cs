5330d406abe7fd6130c7e896e0c53bdded6165a3
Mirai <135774287+MiraiWeLove@users.noreply.github.com>
2026-01-31 05:43:52 +0500

    Camera Controller Script Added
    Made it so the camera moves with the mouse

---
+++ Assets/_Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        Vector3 rot = transform.localRotation.eulerAngles;
+        yRotation = rot.y;
+        xRotation = rot.x;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY; 
+        yRotation += mouseX;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+}
